77a80fcd113de18ec24657cd8ce9665ea38b9b51
Vicente Muñoz Vendrell <vmjob@hotmail.com>
2026-02-21 16:17:24 +0100

    SpawnManager
    Se ha implementado un sistema de Spawn que instancia enemigos. Utiliza un array de enemigos e instancia de manera aleatoria alguno de ellos en una posición a su vez aleatoria.

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -3,10 +3,16 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     [SerializeField] private float topBound = 30.0f;
+    [SerializeField] private float lowerBounds = -10.0f;
+
     // Update is called once per frame
     void Update()
     {
         if(transform.position.z >  topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBounds)
         {
             Destroy(gameObject);
         }
