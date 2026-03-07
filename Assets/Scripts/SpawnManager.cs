3a4677835e0777aa8bdd086adeb46bc9212c3953
Vicente Muñoz Vendrell <vmjob@hotmail.com>
2026-03-07 21:38:02 +0100

    Spawner y DetectCollisions
    Se ha modificado el sistema de spawn utilizando InvokeRepeating para que el spawn sea automático. Se han añadido colliders y se han implementado la detección de colisiones.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,22 +5,26 @@ public class SpawnManager : MonoBehaviour
     [SerializeField] private GameObject[] animalPrefabs;
     private float spawnRangeX = 20.0f;
     private float spawnPosZ = 20.0f;
+    private float spawnDelay = 2.0f;
+    private float spawnInterval = 1.5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", spawnDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        
+    }
 
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation );
-        }
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
